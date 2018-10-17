f856140aceb2a6a75eb4634d9f18b1e5fd869edd
Branko Djordjevic <brankodj89@gmail.com>
2018-10-17 19:27:07 +0200

    returned old script process.py to scri[ts folder

--- scripts/process.py
+++ scripts/process.py
@@ -1,94 +1,61 @@
-from dataflows import Flow, PackageWrapper, validate, delete_fields
-from dataflows import add_metadata, dump_to_path, load, set_type, printer
+import os
+import urllib.request
+import logging
+import csv
+
+downloaded = 'cache/bbk_WU5500.csv'
+outpath = 'data/data.csv'
+source_url = 'http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.M.XAU.USD.EA.AC.C06&its_csvFormat=en&its_fileFormat=csv&mode=its'
+
+def download():
+    if not os.path.exists('cache'):
+        os.makedirs('cache')
+    urllib.request.urlretrieve(source_url, downloaded)
+
+def extract():
+    reader = csv.reader(open(downloaded))
+    newrows = [ row for row in reader ]
+    # skip top 5 rows
+    newrows = newrows[5:]
+    # trim the notes from the bottom
+    newrows = newrows[:-1]
+    # fix up the data
+    # dates are 1968-06 without day ...
+    newrows = [ [row[0] + '-01', row[1]] for row in newrows ]
+
+    existing = []
+    if os.path.exists(outpath):
+        fo = open(outpath)
+        existing = [ row for row in csv.reader(fo) ]
+        fo.close()
+    
+    starter = newrows[0]
+    for idx,row in enumerate(existing):
+        if row[0] == starter[0]:
+            # remove all rows from here on
+            del existing[idx:]
+            break
+    # and now add in new data
+    outrows = existing + newrows
+    fo = open(outpath, 'w')
+    writer = csv.writer(fo)
+    writer.writerows(outrows)
+    fo.close()
+
+import os
+os.environ['http_proxy'] = ''
+def upload():
+    import datastore.client as c
+    dsurl = 'http://datahub.io/dataset/gold-prices/resource/b9aae52b-b082-4159-b46f-7bb9c158d013'
+    client = c.DataStoreClient(dsurl)
+    client.delete()
+    client.upload(outpath)
 
 
-def extract_december_rows(rows):
-    for row in rows:
-        if '-12' in row['Date']:
-            yield row
-
-
-def rename(package: PackageWrapper):
-    package.pkg.descriptor['resources'][0]['name'] = 'gold-prices-annual'
-    package.pkg.descriptor['resources'][0]['path'] = 'data/annual.csv'
-    package.pkg.descriptor['resources'][1]['name'] = 'gold-prices-monthly'
-    package.pkg.descriptor['resources'][1]['path'] = 'data/monthly.csv'
-    yield package.pkg
-    res_iter = iter(package)
-    for res in res_iter:
-        yield res.it
-    yield from package
-
-
-# download_csv_resource()
-gold_price_flow = Flow(
-    add_metadata(
-        name="gold-prices",
-        title="Gold Prices",
-        homepage='http://www.bundesbank.de',
-        licenses=[
-            {
-                "id": "odc-pddl",
-                "name": "public_domain_dedication_and_license",
-                "version": "1.0",
-                "url": "http://opendatacommons.org/licenses/pddl/1.0/"
-            }
-        ],
-        sources=[
-            {
-              "name": "bundesbank-gold-prices",
-              "path": "'http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.M.XAU.USD.EA.AC.C06&its_csvFormat=en&its_fileFormat=csv&mode=its'",
-              "title": "Bundesbank gold prices"
-            }
-        ],
-        views=[
-            {
-                "name": "graph",
-                "title": "Gold Prices (Monthly in USD)",
-                "specType": "simple",
-                "spec": {
-                    "type": "lines-and-points",
-                    "group": "Date",
-                    "series": [
-                        "Price"
-                    ]
-                }
-            }
-        ],
-        related=[
-            {
-                "title": "Oil prices",
-                "path": "/core/oil-prices",
-                "publisher": "core",
-                "formats": ["CSV", "JSON"]
-            },
-            {
-                "title": "Natural gas",
-                "path": "/core/natural-gas",
-                "publisher": "core",
-                "formats": ["CSV", "JSON"]
-            }
-        ],
-        version="0.2.0"
-    ),
-    load(
-        load_source='http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.M.XAU.USD.EA.AC.C06&its_csvFormat=en&its_fileFormat=csv&mode=its',
-        skip_rows=[1, 2, 3, 4, 5, -1],
-        headers=['Date', 'Price', 'Empty column']
-    ),
-    extract_december_rows,
-    load(
-        load_source='http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.M.XAU.USD.EA.AC.C06&its_csvFormat=en&its_fileFormat=csv&mode=its',
-        skip_rows=[1, 2, 3, 4, 5, -1],
-        headers=['Date', 'Price', 'Empty column']
-    ),
-    rename,
-    set_type('Date', resources='gold-prices-annual', type='yearmonth', format='any'),
-    set_type('Price', resources='gold-prices-annual', type='number', format='any'),
-    set_type('Date', resources='gold-prices-monthly', type='yearmonth', format='any'),
-    set_type('Price', resources='gold-prices-monthly', type='number', format='any'),
-    validate(),
-    delete_fields(['Empty column'], resources=None),
-    dump_to_path(),
-)
-gold_price_flow.process()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    print('Downloading')
+    download()
+    print('Extracting and merging')
+    extract()
+    
